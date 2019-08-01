f329d6ac140e563c0b0edfefa4ca0a0b3b780488
Dear丶恒 <492238647@qq.com>
2019-08-01 17:42:56 +0800

    删除文件夹

--- del.js
+++ del.js
@@ -16,7 +16,7 @@ function deleteFolder(path) {
         fs.rmdirSync(path);
     }
 }
-var arr_wx = ['MTU0Njc1NDYwMDYwOS3nlJ_lj5E=','MTU0NjU2NTk4ODA2Ni3nlLfnp5E=','MTU0NjU4MTUwMTM4NC3nmb3phZI=','MTU1NTQ2OTk4MzgyMy3lh4_ogqU=','MTU0Njc1NDYwMDYwOS3nlJ_lj5E=','MTU0Njk0NDMyNTI2Mi3pu5Hlj5E=','MTU0NzAwMjExNzg2NC3npZvmlpE=','MTU0NzAxNzc1MzM0OC3kuK3oja8=','MTU0NzAzMDQ1MjMzNS3lh4_ogqU=','MTU0NzExOTQ2NjI3OC3lh4_ogqU=','MTU0NzAxNzc1MzM0OC3kuK3oja8=','MTU0NzE5OTI0MTk5My3lh4_ogqU=','MTU0NzE5OTI0MjA5NS3lh4_ogqU=','MTU0NzE5OTI0MjE5Ni3lh4_ogqU=','MTU0NzE5OTI0MjI5OC3lh4_ogqU=','MTU0NzE5OTI0MjM5OS3lh4_ogqU=','MTU1NTMxMjA2NzAzNi3nlLfnp5E=','MTU1NTMxMjQ4ODYyNC3nlLfnp5E=','MTU1NTMxMjU1MzE2OS3lh4_ogqU=','MTU1NTMxMjY3OTEzMi3nlLfnp5E=','MTU1NTQwNzcxNDY3OS3lh4_ogqU=','MTU1NTU2OTMyMzUzMy3lh4_ogqU=','MTU1NTY0MDE4MzY0Mi3lh4_ogqU=','MTU1NTY0MDE5MjIwOS3lh4_ogqU=','MTU2MTUyOTc3OTcxNS3lh4_ogqU='];
+var arr_wx = ['GetgXzMV', 'yhj45sd1gth'];
 
 arr_wx.forEach(item => {
   let path=p.join(__dirname,"./" + item);
