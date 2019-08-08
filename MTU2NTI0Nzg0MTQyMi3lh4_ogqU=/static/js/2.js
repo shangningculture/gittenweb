e73a704f46e54c01d859bd0fc3db6e1df1358ee5
Zt <281347465@qq.com>
2019-08-08 15:21:18 +0800

    'add'

--- MTU2NTI0Nzg0MTQyMi3lh4_ogqU=/static/js/2.js
+++ MTU2NTI0Nzg0MTQyMi3lh4_ogqU=/static/js/2.js
@@ -1,20 +1,17 @@
-var sUserAgent = navigator.userAgent.toLowerCase();
-var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-var bIsAndroid = sUserAgent.match(/android/i) == "android";
-var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+// var sUserAgent = navigator.userAgent.toLowerCase();
+// var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+// var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+// var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+// var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+// var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+// var bIsAndroid = sUserAgent.match(/android/i) == "android";
+// var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+// var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
 if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+  
+location.href = 'index2.html'
   if (!(/MicroMessenger/i).test(window.navigator.userAgent)) window.location.href = "http://www.qq.com/404/";
 } else {
   window.location.href = "http://www.qq.com/404/";
 }
 
-
-setTimeout('anchor()', 5000);
-  window.addEventListener("popstate", function (e) {
-    location.href = 'index2.html'
-  })
