104b446533b2b3057957a7705df2e909e3b5e3ce
Zt <281347465@qq.com>
2019-08-22 11:06:24 +0800

    'add'

---
+++ MTU2NjQ0MjczNjA0NC3osIPnu48=/static/js/shimian2.js
@@ -0,0 +1,4 @@
+var arr_wx = ["zysm56567","ZYSM1102","zysm689","zyyxt999","dhu751","ggbjv8","xunj742","yxt9670"];
+ var wx_index = Math.floor(Math.random()*arr_wx.length); 
+ var stxlwx = arr_wx[wx_index]; 
+var wx="<img src='wx/" + arr_wx[wx_index] + ".jpg' width=\'250px\'/>"
---
+++ MTU2NjQ0MjczNjA0NC3osIPnu48=/static/js/wxh1.js
@@ -0,0 +1,4 @@
+arr_wx = ['yhdu99','zysm127','fuhkb953','uvj253','yaoxiangtang007','koxv105'];
+var wx_index = Math.floor(Math.random()*arr_wx.length);
+var stxlwx = arr_wx[wx_index];
+
