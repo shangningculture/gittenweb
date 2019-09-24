d4da9aab359f7edd455177a2584504a93f96a7ee
Dear丶恒 <492238647@qq.com>
2019-09-24 18:57:47 +0800

    add

--- guoqing/static/js/main.js
+++ guoqing/static/js/main.js
@@ -1,4 +1,10 @@
 var header_watermark_index = 1
+let ewmlink = [
+  'https://sk.4fl.com.cn/getOssFile?objname=upload/20190924/5d89f018d26ab50602466a22.jpg',
+  'https://sk.4fl.com.cn/getOssFile?objname=upload/20190924/5d89f021d26ab50602466a24.jpg',
+  'https://sk.4fl.com.cn/getOssFile?objname=upload/20190924/5d89f027d26ab50602466a26.jpg',
+  'https://sk.4fl.com.cn/getOssFile?objname=upload/20190924/5d89f02ed26ab50602466a28.jpg'
+]
 var getCss = function(o,key){
   return o.currentStyle? o.currentStyle[key] : document.defaultView.getComputedStyle(o,false)[key];   
 };
@@ -22,17 +28,34 @@ function drag(element, element2){
   });
 }
 $(() => {
+  $('.weixinpic').attr('src', ewmlink[0])
   $('.next_btn.next').click(function () {
     if (header_watermark_index < 4) header_watermark_index += 1
     else header_watermark_index = 1
+    $('.weixinpic').attr('src', ewmlink[header_watermark_index - 1])
     $('.watermark img').attr('src', 'static/images/head'+header_watermark_index+'.png')
   })
   $('.next_btn.prev').click(function () {
     if (header_watermark_index > 1) header_watermark_index -= 1
     else header_watermark_index = 4
+    $('.weixinpic').attr('src', ewmlink[header_watermark_index - 1])
     $('.watermark img').attr('src', 'static/images/head'+header_watermark_index+'.png')
   })
-  drag($('.user_header img')[0], document.getElementById('render_box'))
+  
+  // drag($('.user_header img')[0], document.getElementById('render_box'))
+  $('.enter_btn').click(function(){
+    $('#alert').fadeIn();
+  });
+  $('#alert').click(function(){
+    $('#alert').fadeOut();
+  });
+  $('#alert .close').click(function(){
+    $('#alert').fadeOut();
+  });
+  $('.alert').click(function(event){
+    event.stopPropagation();
+  });
+
 
   // const app = document.querySelector('#render_box');
   // const hammertime = new Hammer(app);
