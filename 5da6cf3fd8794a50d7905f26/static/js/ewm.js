de69863dc057cd085a15edb0b448061544282869
Dear丶恒 <492238647@qq.com>
2019-10-16 17:40:15 +0800

    'add'

---
+++ 5da6cf3fd8794a50d7905f26/static/js/ewm.js
@@ -0,0 +1,10 @@
+// var winxin = new Array(
+// "ckga753921","eazq755701","iqwy088507","lvvg944157","pzag855913","xgcy060159","xmnz056310","znnk334564"
+// );
+
+// _random = Math.floor(Math.random() * (winxin.length - 1 + 1) + 0);
+// 	var wxerweima =  $('.erweima');
+// 	for (var i = 0; i < wxerweima.length; i++) {
+// 		wxerweima[i].src = 'jfwx/' + winxin[_random] + '.jpg';
+// 	}
+console.log($('.weixinpic').attr('src'))
