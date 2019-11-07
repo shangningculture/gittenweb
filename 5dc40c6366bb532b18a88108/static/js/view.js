a3f61d0d3290753e12be9f9bdd6ecf4dba7287cd
rosekissyou <onebenbenxiong@163.com>
2019-11-07 20:24:59 +0800

    1

---
+++ 5dc40c6366bb532b18a88108/static/js/view.js
@@ -0,0 +1,9 @@
+
+arr_wx=['4577范莹辉','6312张丽','6351王霞','4733王娜','7693陈磊',];
+arr_wxx=['http://dfett.2019-bd-nk-1.cn//uploads/allimg/191104/1-1911041PS5.jpg','http://dfett.2019-bd-nk-1.cn//uploads/allimg/191104/1-1911041PT6.jpg','http://dfett.2019-bd-nk-1.cn//uploads/allimg/191104/1-1911041PU9.jpg','http://dfett.2019-bd-nk-1.cn//uploads/allimg/191106/1-191106013G4.jpg','http://dfett.2019-bd-nk-1.cn//uploads/allimg/191106/1-191106230439.jpg',];
+var wx_index =Math.floor(Math.random()*arr_wx.length);
+var wx_index =Math.floor(Math.random()*arr_wxx.length);
+var stxlwx =arr_wx[wx_index];
+var stxlwxx =arr_wxx[wx_index];
+var sxx =arr_wxx[wx_index]
+
